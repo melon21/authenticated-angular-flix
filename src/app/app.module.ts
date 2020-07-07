f49761f513acc207ce6df57ca77a547fc9396141
Melinda Marino <melindamarino22@gmail.com>
2020-07-07 03:14:23 -0500

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +16,7 @@ import {MovieService} from './movie.service';
 import {MovieApiService} from './movie-api.service';
 import {ApiService} from './api.service';
 import { SignUpLoginComponent } from './sign-up-login/sign-up-login.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { SignUpLoginComponent } from './sign-up-login/sign-up-login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [MovieService,MovieApiService,ApiService,],
   bootstrap: [AppComponent]
--- src/app/sign-up-login/sign-up-login.component.ts
+++ src/app/sign-up-login/sign-up-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-login',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-login.component.css']
 })
 export class SignUpLoginComponent implements OnInit {
-
-  constructor() { }
+username = '';
+password = '';
+  constructor(private authService: AuthenticationService, private routerService: Router) { }
 
   ngOnInit() {
   }
+async login() {
+await this.authService.logIn(this.username, this.password);
+if (!this.authService.isAuthenticated()) {
+  console.log('Unable to login');
+} else{
+  this.routerService.navigate(['/main']);
+}
+}async signUp() {
+  await this.authService.signUp(this.username, this.password);
+  await this.login();
+}
 
 }
